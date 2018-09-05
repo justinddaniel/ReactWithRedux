3a72d2a5f932563ce206dccd574ca8c4a97a10a9
Justin Daniel <justinddaniel@gmail.com>
2018-09-05 19:18:41 -0400

    Turn book-list into container component; import connect, wire mapStateToProps, wire connect to mapStateToProps and BookList

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'; //import connect property from react-redux
 
-export default class Booklist extends Component {
+class Booklist extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -17,7 +18,21 @@ export default class Booklist extends Component {
   }
 }
 
-
 //container is a component that has direct access to the state produced by Redux
 //React and Redux are two separate libraries. react-redux melds them together.
-//only the most parent component that uses a particular state needs to connect to Redux. 
+//only the most parent component that uses a particular state needs to connect to Redux.
+
+function mapStateToProps(state) { //the "glue" between React and Redux
+  return {
+    books: state.books //whatever is returned is available to component as this.props
+  };
+}
+
+//takes application state, whatever is returned will show up as props inside of BookList
+//containers are link between React and Redux through the mapStateToProps function.
+
+export default connect(mapStateToProps)(BookList);
+//connect takes a function and component, and produces a container. container is aware of the state in Redux.
+
+//whenver our application state changes, the container will re-render.
+//whenver the application state changes, the object assigned will change this.props. 
