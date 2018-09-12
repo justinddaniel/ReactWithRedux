49bca4c5fa7f933a2248146387445d870b7a104f
Justin Daniel <justinddaniel@gmail.com>
2018-09-12 14:18:31 -0400

    Wired mapDispatchToProps to allow data to flow to reducers

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'; //import connect property from react-redux
+import { selectBook} from '../actions/index';
+import { bindActionCreators } from 'redux'; //used to ensure action genrated by actioncreator flows through reducers;
 
 class Booklist extends Component {
   renderList() {
@@ -31,8 +33,17 @@ function mapStateToProps(state) { //the "glue" between React and Redux
 //takes application state, whatever is returned will show up as props inside of BookList
 //containers are link between React and Redux through the mapStateToProps function.
 
-export default connect(mapStateToProps)(BookList);
+
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch); //allows dispatch to flow through reducers. 
+}
+
+//Promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make it available as a prop.
+//React-redux documentation is a good resource for connect function.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 //connect takes a function and component, and produces a container. container is aware of the state in Redux.
 
 //whenver our application state changes, the container will re-render.
-//whenver the application state changes, the object assigned will change this.props. 
+//whenver the application state changes, the object assigned will change this.props.
