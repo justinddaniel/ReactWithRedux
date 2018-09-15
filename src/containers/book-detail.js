5ee7fa3fc68fcda8e976bcee9b9c19a8387ba468
Justin Daniel <justinddaniel@gmail.com>
2018-09-15 15:46:54 -0400

    use conditional to check if null

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,8 +3,14 @@ import {connect} from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) { //often used to deal with initial state of null; prevents errors for this.props.book.title.
+      return <div>Select a book to get started.</div>
+    }
     return (
-      <div>Book Detail!</div>
+      <div>
+       <h3>Details for:</h3>
+       <div>{this.props.book.title}</div>
+      </div>
     );
   }
 }
